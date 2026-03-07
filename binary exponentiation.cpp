38f3c64ff05ed70a793bb19acc96b934541700ce
Kunal Pawar <139967951+kunalpawar04@users.noreply.github.com>
2026-03-07 08:25:23 +0530

    Add modulo to prevent overflow

--- binary exponentiation.cpp
+++ binary exponentiation.cpp
@@ -2,10 +2,10 @@ long long binaryExponentiation(long long base, long long exponent) {
     long long result = 1;
     while (exponent > 0) {
         if (exponent % 2 == 1) {
-            result *= base;
+            result = (result * base) % 1000000007;
         }
-        base *= base; // Square the base
-        exponent /= 2; // Divide the exponent by 2
+        base = (base * base) % 1000000007; // Square the base
+        exponent /= 2;                     // Divide the exponent by 2
     }
     return result;
 }
